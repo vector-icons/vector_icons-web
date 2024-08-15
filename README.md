46d09c463acac819188601a8ce0509c2c52fb512
Dev TtangKong <ttankkeo112@gmail.com>
2024-08-15 14:54:17 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+> [!IMPORTANT]
+> Sorry, i'm developing about this template (start-kit).
+
+<div align="center">
+  <img src="https://github.com/user-attachments/assets/2260123b-0f99-4d9d-a10e-978ed968c293">
+  <h1>Preact Typescript With Node</h1>
+  <span>This is template that is simple start-kit about Preact in Node with Typescript.</span>
+</div>
---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+import mime from "mime-types";
+
+const contentTypeOf = (path: string) => {
+    return mime.lookup(path) || "application/octet-stream";
+}
+
+const server = http.createServer((request, response) => {
+    if (request.url === undefined) return;
+
+    // If the file extension does not exist,
+    // it will respond with `index.html` due to the characteristics of SPA.
+    if (request.method == "GET" && /\.\w+$/.test(request.url)) {
+        const srcPath = path.join("../client/dist", path.normalize(request.url));
+
+        fs.readFile(srcPath, null, (err, data) => {
+            if (err) { // Is not found or other errors occur.
+                response.writeHead(404);
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": contentTypeOf(srcPath)});
+                response.end(data);
+            }
+        });
+    } else {
+        fs.readFile("../client/dist/index.html", "utf8", (err, data) => {
+            response.writeHead(200, {"Content-Type": contentTypeOf("html")});
+            response.end(data);
+        });
+    }
+});
+
+server.listen(8080, () => {
+    console.log("Hello, World!");
+});
