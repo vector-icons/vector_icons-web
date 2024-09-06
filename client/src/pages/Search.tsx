e55a52b739388155c3f6112baa43d8a23f439e4a
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-06 18:50:50 +0900

    Update client files about sidebar development

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,10 +1,11 @@
-import { AnimatedFoldable, Box, Column, Row, Scrollable, Text } from "react-widgets";
+import { AnimatedFoldable, Box, Column, Row, Scrollable, TabNavigation, Text } from "react-widgets";
 import { RenderIcon } from "../templates/RenderIcon";
 import { Icons } from "./App";
 import { TouchRipple } from "web-touch-ripple/jsx";
 
 import Logo from "../assets/icons/logo.svg";
 import { Container } from "../templates/Container";
+import { useState } from "preact/hooks";
 
 export function SearchPage() {
     return (
@@ -19,15 +20,56 @@ export function SearchPage() {
 }
 
 function SideBar() {
+    const [index, setIndex] = useState(0); // temp
+
     return (
-        <Column flexShrink="0" padding="var(--padding-df)" backgroundColor="var(--rearground)" borderRight="1px solid var(--rearground-border)">
+        <Column
+            flexShrink="0"
+            gap="var(--padding-df)"
+            padding="var(--padding-df)"
+            backgroundColor="var(--rearground)"
+            borderRight="1px solid var(--rearground-border)"
+        >
             <Row center gap="var(--padding-sm)" padding="0px var(--padding-df)">
                 <Logo width="20px" />
                 <AnimatedFoldable.Horizontal visible={true} duration="0.3">
                     <Text.h1 fontSize="22px">Icons Search</Text.h1>
                 </AnimatedFoldable.Horizontal>
             </Row>
+            <TabNavigation.Vertical
+                index={index}
+                duration="0.3s"
+                style={{borderRadius: "1e10px", backgroundColor: "var(--foreground2)", width: "50%"}}
+            >
+                <SizeBarItem selected={index == 0} onTap={() => setIndex(0)} iconName="home" title="Home" />
+                <SizeBarItem selected={index == 1} onTap={() => setIndex(1)} iconName="community" title="Community" />
+                <SizeBarItem selected={index == 2} onTap={() => setIndex(2)} iconName="storage" title="Storage" />
+                <SizeBarItem selected={index == 3} onTap={() => setIndex(3)} iconName="settings" title="Settings" />
+            </TabNavigation.Vertical>
         </Column>
+    )
+}
+
+function SizeBarItem({selected, iconName, title, onTap}: {
+    selected: boolean;
+    iconName: string;
+    title: string;
+    onTap: VoidFunction;
+}) {
+    return (
+        <TouchRipple onTap={onTap}>
+            <Row centerLeft padding="var(--padding-df)" borderRadius="10px">
+                <RenderIcon.Name filled={selected} size="18px" name={iconName} />
+                <Box
+                    width="100%"
+                    marginLeft="var(--padding-df)"
+                    color={selected ? "var(--foreground)" : "var(--foreground3)"}
+                    transitionProperty="color"
+                    transitionDuration="0.3s"
+                    children={title}
+                />
+            </Row>
+        </TouchRipple>
     )
 }
 
--- client/src/templates/RenderIcon.tsx
+++ client/src/templates/RenderIcon.tsx
@@ -1,4 +1,5 @@
 import { useLayoutEffect, useRef } from "preact/hooks";
+import { Icons } from "../pages/App";
 
 export function RenderIcon({size, innerHTML}: {
     size: string,
@@ -13,9 +14,21 @@ export function RenderIcon({size, innerHTML}: {
         wrapper.style.width = size;
         wrapper.style.height = size;
         wrapper.style.justifyContent = "center";
-    }, []);
+    });
 
-    return (
-        <div ref={wrapperRef} />
-    )
+    return <div ref={wrapperRef} />
+}
+
+export namespace RenderIcon {
+    export function Name({size, name, filled = false}: {
+        size: string,
+        name: string,
+        filled?: boolean
+    }) {
+        const icon = Icons.find(i => i.name == name).content;
+        const iconNormal = icon.normal;
+        const iconFilled = icon.filled;
+
+        return <RenderIcon size={size} innerHTML={filled ? iconFilled : iconNormal} />
+    }
 }
