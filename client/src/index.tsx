e135378473c71a1c3756a800470401405b860fcf
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-08 14:15:09 +0900

    Update client files to about dark mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,4 +9,9 @@ import "./styles/templates";
 import "./components/landing_background";
 import "web-touch-ripple";
 
+// Insert className dark theme into body element when a user is dark mode.
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.className = "dark";
+}
+
 addEventListener("load", () => render(<App />, document.body));
