a86515709db19708c7ec62293f0fd187c55eb314
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-08 17:01:22 +0900

    Update client files to fade animation about initial-page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,8 +5,10 @@ import "./styles/main";
 import "./styles/font";
 import "./styles/variables";
 import "./styles/templates";
+import "./styles/keyframes";
 
 import "./components/landing_background";
+
 import "web-touch-ripple";
 
 // Insert className dark theme into body element when a user is dark mode.
@@ -14,4 +16,11 @@ if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").match
     document.body.className = "dark";
 }
 
-addEventListener("load", () => render(<App />, document.body));
+addEventListener("load", () => {render(<App />, document.body)});
+addEventListener("DOMContentLoaded", () => {
+    document.body.style.animation = "page-fadein 0.3s";
+});
+
+addEventListener("beforeunload", () => {
+    document.body.style.animation = "page-fadeout 0.3s";
+});
