cb51ebe9b90d0a95357a1c2cf89818d8a6ea1f4b
Dev Ttangkong <ttankkeo112@gmail.com>
2024-10-13 10:18:57 +0900

    Update client files to about storage system

--- client/src/components/user.ts
+++ client/src/components/user.ts
@@ -3,10 +3,10 @@ import { Storage } from "./storage";
 export class User {
     // 북마크된 아이콘의 이름을 리스트 형태로 반환합니다.
     static get markedIcons(): string[] {
-        return Storage.get("user:marked_icons") ?? [];
+        return Storage.get("user-marked_icons") ?? [];
     }
 
     static set markedIcons(values: string[]) {
-        Storage.set("user:marked_icons", values);
+        Storage.set("user-marked_icons", values);
     }
 }
