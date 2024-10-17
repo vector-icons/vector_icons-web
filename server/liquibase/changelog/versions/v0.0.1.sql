86b451b6584569a88f0b311759fd7a2fc8f30346
Dev Ttangkong <ttankkeo112@gmail.com>
2024-10-18 04:36:35 +0900

    Fixed an issue about liquibase and create db-table as 'User'

---
+++ server/liquibase/changelog/versions/v0.0.1.sql
@@ -0,0 +1,24 @@
+
+CREATE DOMAIN UUID AS VARCHAR(36);
+
+CREATE DOMAIN SHA512 AS CHAR(88);
+
+CREATE DOMAIN sha256 AS CHAR(44);
+
+CREATE TABLE "User"(
+    "id" UUID PRIMARY KEY,
+    "createdAt" TIMESTAMP NOT NULL,
+    "updatedAt" TIMESTAMP,
+    "deletedAt" TIMESTAMP,
+    "displayName" VARCHAR(64),
+    "alias" VARCHAR(128) NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" SHA512 NOT NULL,
+    "passwordSalt" SHA256 NOT NULL
+);
+
+CREATE INDEX "indexAlias" ON "User"("alias");
+
+CREATE INDEX "indexEmail" ON "User"("email");
+
+CREATE INDEX "indexCreatedAt" ON "User"("createdAt");
