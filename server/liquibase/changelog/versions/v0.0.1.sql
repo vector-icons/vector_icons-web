9d3e597182994b12ff262b56f4b9a08d189e896d
Dev Ttangkong <ttankkeo112@gmail.com>
2024-10-30 19:50:39 +0900

    Update server files to modifying UserDetails table columns

--- server/liquibase/changelog/versions/v0.0.1.sql
+++ server/liquibase/changelog/versions/v0.0.1.sql
@@ -3,7 +3,24 @@ CREATE DOMAIN UUID AS VARCHAR(36);
 
 CREATE DOMAIN SHA512 AS CHAR(88);
 
-CREATE DOMAIN sha256 AS CHAR(44);
+CREATE DOMAIN SHA256 AS CHAR(44);
+
+CREATE DOMAIN URL AS VARCHAR(255);
+
+CREATE DOMAIN RGB AS CHAR(11); -- 0~255,0~255,0~255
+
+CREATE FUNCTION "random_rgb"(min INT DEFAULT 0, max INT DEFAULT 255) RETURNS RGB AS $$
+DECLARE
+    r INT;
+    g INT;
+    b INT;
+BEGIN
+    r := floor(random() * (max - min + 1) + min);
+    g := floor(random() * (max - min + 1) + min);
+    b := floor(random() * (max - min + 1) + min);
+    RETURN r || ',' || g || ',' || b;
+END;
+$$ LANGUAGE PLPGSQL;
 
 CREATE TABLE "User"(
     "id" UUID PRIMARY KEY,
@@ -25,7 +42,11 @@ CREATE INDEX "indexCreatedAt" ON "User"("createdAt");
 
 CREATE TABLE "UserDetails"(
     "id" UUID NOT NULL,
-    "markedIcons" JSON NOT NULL DEFAULT '[]',
-    "importIcons" JSON NOT NULL DEFAULT '[]',
+    "bookmarkedIcons" JSON NOT NULL DEFAULT '[]',
+    "downloadedIcons" JSON NOT NULL DEFAULT '[]',
+    "profileColor" RGB NOT NULL DEFAULT random_rgb(100, 255),
+    "profileImage" URL,
     FOREIGN KEY("id") REFERENCES "User"("id") ON DELETE CASCADE
 );
+
+INSERT INTO "UserDetails"("id") VALUES('91138b80-e996-41e0-a6f4-79b391537fa6');
